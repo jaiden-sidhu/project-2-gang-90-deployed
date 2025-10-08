9a6e135fc0d677e90aa4ab838b50f9fb2cc18844
balar0 <144611586+balar0@users.noreply.github.com>
2025-10-07 21:39:20 -0500

    added trasaction class

--- pos-app/src/main/java/edu/tamu/project2/csce331/Transaction.java
+++ pos-app/src/main/java/edu/tamu/project2/csce331/Transaction.java
@@ -1,8 +1,19 @@
 package edu.tamu.project2.csce331;
 
+import java.security.Timestamp;
+
 public class Transaction {
-    Transaction(){
-        
+    public int transaction_id;
+    public String customer_name;
+    public Timestamp transaction_time;
+    public int employee_id;
+    public double total_price;
+    Transaction(int transaction_id, String customer_name, Timestamp transaction_time, int employee_id, double total_price ){
+        this.transaction_id = transaction_id;
+        this.customer_name = customer_name;
+        this.transaction_id = transaction_id;
+        this.employee_id = employee_id;
+        this.total_price = total_price;
     }
     
 }
