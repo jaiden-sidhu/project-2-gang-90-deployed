90be00e08b1ce37ba30775ac640e7559921c6adf
sharksback <mrsharksback@tamu.edu>
2025-10-08 20:17:22 -0500

    TransactionHistoryController switch

--- pos-app/src/main/java/edu/tamu/project2/csce331/TransactionsHistoryController.java
+++ pos-app/src/main/java/edu/tamu/project2/csce331/TransactionsHistoryController.java
@@ -3,13 +3,18 @@ package edu.tamu.project2.csce331;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 // Using Text in FXML for status label
 import javafx.scene.control.Pagination;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -68,6 +73,63 @@ public class TransactionsHistoryController {
             statusLabel.setText(String.format("Showing %d of %d total", data.size(), totalCount));
         } catch (Exception e) {
             statusLabel.setText("Failed to load page: " + e.getMessage());
+        }
+    }
+
+    public void go_manage_employee() 
+    { 
+        try 
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("project-2-gang-90\\pos-app\\src\\main\\resources\\edu\\tamu\\project2\\csce331\\employee_list.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) statusLabel.getScene().getWindow();
+
+            stage.setScene(new Scene(root));
+            stage.setTitle("Manager - Employee");
+            stage.show();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void go_products() 
+    { 
+        try 
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("project-2-gang-90\\pos-app\\src\\main\\resources\\edu\\tamu\\project2\\csce331\\manager_products.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) statusLabel.getScene().getWindow();
+
+            stage.setScene(new Scene(root));
+            stage.setTitle("Manager - Products");
+            stage.show();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void go_cashier() 
+    { 
+        try 
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("project-2-gang-90\\pos-app\\src\\main\\resources\\edu\\tamu\\project2\\csce331\\cashier_menu.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) statusLabel.getScene().getWindow();
+
+            stage.setScene(new Scene(root));
+            stage.setTitle("Cashier - Menu");
+            stage.show();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
         }
     }
 }
